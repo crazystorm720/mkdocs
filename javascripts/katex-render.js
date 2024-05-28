42f2e6fa1877f1e75d0bb8d7f9029818799c00b4
Whisker Jones <medusa@aphrodite.atlantis.lan>
2024-05-28 08:43:36 -0600

    Deployed 4c0de10 with MkDocs version: 1.6.0

---
+++ javascripts/katex-render.js
@@ -0,0 +1,12 @@
+document$.subscribe(() => {
+    renderMathInElement(document.body, {
+      delimiters: [
+        {left: "$$", right: "$$", display: true},
+        {left: "\\[", right: "\\]", display: true},
+        {left: "$", right: "$", display: false},
+        {left: "\\(", right: "\\)", display: false}
+      ],
+      throwOnError: false
+    });
+  });
+  
